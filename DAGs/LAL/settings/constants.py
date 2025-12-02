36d930b7127645eff3c5cfcb3b2e3efd2101c051
svetlana-terzeman <terzeman01@gmail.com>
2025-12-02 17:35:58 +0300

    Update constance LAL

--- DAGs/LAL/settings/constants.py
+++ DAGs/LAL/settings/constants.py
@@ -2,51 +2,33 @@ import os
 from dotenv   import load_dotenv
 load_dotenv("/app/.env")
 
-# IP       = os.getenv('IP_DATA', '83.239.206.206')
-# PORT     = int(os.getenv('PORT_DATA', 8011))          # HTTP-порт (8123 в контейнере)
-# DB       = os.getenv('DB_DATA', 'ASH')
-# USERNAME = os.getenv('USERNAME_DATA', 'user6525')
-# PASSWD   = os.getenv('PASSWD_DATA', '34SwkLaaEi7aHfj0')
-IP       = '83.239.206.206'
-PORT     = 8011          # HTTP-порт (8123 в контейнере)
-DB       = 'ASH'
-USERNAME = 'user6525'
-PASSWD   = '34SwkLaaEi7aHfj0'
+IP       = os.getenv('IP_DATA', '83.239.206.206')
+PORT     = int(os.getenv('PORT_DATA', 8011))          # HTTP-порт (8123 в контейнере)
+DB       = os.getenv('DB_DATA', 'ASH')
+USERNAME = os.getenv('USERNAME_DATA', 'user6525')
+PASSWD   = os.getenv('PASSWD_DATA', '34SwkLaaEi7aHfj0')
 
-# HOST_BI     =  os.getenv('DB_HOST', 'db')
-# DB_BI       =  os.getenv('DB_NAME', 'BI')
-# USERNAME_BI =  os.getenv('DB_USER', 'bitask_user')
-# PASSWD_BI   =  os.getenv('DB_PASS', 'bitask_pass')
-# tablename   =  os.getenv('BI_tablename', 'bi__task_register')
-HOST_BI     =  'db'
-DB_BI       =  'BI'
-USERNAME_BI =  'bitask_user'
-PASSWD_BI   =  'bitask_pass'
-tablename   =  'bi__task_register'
 
-# RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'rabbitmq')
-# RABBITMQ_USER = os.getenv('RABBITMQ_DEFAULT_USER', 'admin')
-# RABBITMQ_PASS = os.getenv('RABBITMQ_DEFAULT_PASS', 'mq_pass')
-# queue_name    = os.getenv('queue_name_LAL', 'DAGs/LAL')
+HOST_BI     =  os.getenv('DB_HOST', 'db')
+DB_BI       =  os.getenv('DB_NAME', 'BI')
+USERNAME_BI =  os.getenv('DB_USER', 'bitask_user')
+PASSWD_BI   =  os.getenv('DB_PASS', 'bitask_pass')
+tablename   =  os.getenv('BI_tablename', 'bi__task_register')
 
-RABBITMQ_HOST = 'rabbitmq'
-RABBITMQ_USER = 'admin'
-RABBITMQ_PASS = 'mq_pass'
-queue_name    = 'DAGs/LAL'
 
-# SCHEDULE_INTERVAL  = int(os.getenv('SCHEDULE_INTERVAL', 20)) #время между проверками очереди в секундах
-# MAX_PARALLEL_TASKS = int(os.getenv('MAX_PARALLEL_TASKS', 1)) #кол-во параллельных запусков
-SCHEDULE_INTERVAL  = 20 #время между проверками очереди в секундах
-MAX_PARALLEL_TASKS = 1 #кол-во параллельных запусков
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'rabbitmq')
+RABBITMQ_USER = os.getenv('RABBITMQ_DEFAULT_USER', 'admin')
+RABBITMQ_PASS = os.getenv('RABBITMQ_DEFAULT_PASS', 'mq_pass')
+queue_name    = os.getenv('queue_name_LAL', 'DAGs/LAL')
 
-# REVISION        = os.getenv('REVISION', 'v20250522')
-# _correct_coef   = float(os.getenv('_correct_coef', 0.2))
-# table_predict   = os.getenv('table_predict', 'BIG_DATA_LTV_ONLINE_OFFLINE_PREDICT')
-# save_result     = os.getenv('save_result', ['db', 'parquet', 'excel'])  # сохранение результата в бд 'db', в файл 'parquet', 'csv', 'excel'
-REVISION        = 'v20250522'
-_correct_coef   = 0.2
-table_predict   = 'BIG_DATA_LTV_ONLINE_OFFLINE_PREDICT'
-save_result     = ['db', 'parquet', 'excel']
+SCHEDULE_INTERVAL  = int(os.getenv('SCHEDULE_INTERVAL', 20)) #время между проверками очереди в секундах
+MAX_PARALLEL_TASKS = int(os.getenv('MAX_PARALLEL_TASKS', 1)) #кол-во параллельных запусков
+
+REVISION        = os.getenv('REVISION', 'v20250522')
+_correct_coef   = float(os.getenv('_correct_coef', 0.2))
+table_predict   = os.getenv('table_predict', 'BIG_DATA_LTV_ONLINE_OFFLINE_PREDICT')
+save_result     = os.getenv('save_result', ['db', 'parquet', 'excel'])  # сохранение результата в бд 'db', в файл 'parquet', 'csv', 'excel'
+
 
 passport_of_models = [
 
